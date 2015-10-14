04496aea4ed46645286b2f4cd571bd351a117e4e
Alexey Maleykov <a@htmlacademy.ru>
2015-10-14 15:18:28 +0300

    Api и описание

--- Readme.md
+++ Readme.md
@@ -1,2 +1,37 @@
 HTML Academy Services API
 ============
+
+###1 KueApi
+####1.1 Basic usage
+```
+$api = new \Service\KueApi();
+$api->job('job-type', [
+    'param1' => 'value1',
+    'param2' => 'value2'
+]);
+```
+####1.2 Advanced usage
+
+#####1.2.1 Configuration
+```
+$config = [
+  'scheme' => 'http',
+  'host'   => 'localhost',
+  'port'   => 3000,
+  'api'    => '/api'
+];
+$api = new \Service\KueApi($config);
+```
+#####1.2.1 Job parameters
+```
+public int KueApi::job(string $type[, array $data [, mixed $startAt [, string $priority [, int $attempts]]]]):mixed;
+```
+*Parameters*
+**type** - type of created job
+**data** - additional data for job
+**startAt** - string formatted date or DateTime object to specify a delay for executing this job. `null` if job should run immediately (default)
+**priority** - priority of the job. Available values `low`, `normal`, `medium`, `high`, `critical`. Default `normal`
+**attempts** - count of attempts to execute this job. Default `5`
+
+*Return values*
+Returns `id` of created job or `false` if on failure.
--- src/Service/KueApi.php
+++ src/Service/KueApi.php
@@ -4,8 +4,82 @@ namespace Service;
 
 class KueApi
 {
-    public static function test()
+    private $scheme = 'http';
+    private $host = 'localhost';
+    private $port = '3000';
+    private $api = '/api';
+
+    public function __construct($options = [])
     {
-        var_dump('test is here');
+        if (!empty($options['host'])) {
+            $this->host = $options['host'];
+        }
+        if (!empty($options['port'])) {
+            $this->port = $options['port'];
+        }
+        if (!empty($options['api'])) {
+            $this->api = $options['api'];
+        }
+        if (!empty($options['scheme'])) {
+            $this->scheme = $options['scheme'];
+        }
+    }
+
+    public function job($type, $data = [], $start = null, $priority = 'normal', $attempts = 5)
+    {
+        $job = [
+            'type' => $type,
+            'data' => $data,
+            'options' => [
+                'attempts' => $attempts,
+                'priority' => $priority
+            ]
+        ];
+        if (!empty($start)) {
+            try {
+                if (!($start instanceof \DateTime)) {
+                    $start = new \DateTime($start);
+                }
+                $now = new \DateTime();
+
+                $delay = $start->getTimestamp() - $now->getTimestamp();
+                if ($delay > 0) {
+                    $job['options']['delay'] = $delay * 1000;
+                }
+            } catch (\Exception $e) {
+
+            }
+        }
+        return $this->save(json_encode($job));
+    }
+
+    private function save($job)
+    {
+        $curl = curl_init();
+        curl_setopt_array($curl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => sprintf(
+                '%s://%s:%s/%s/job',
+                $this->scheme,
+                $this->host,
+                $this->port,
+                trim($this->api, '/')
+            ),
+            CURLOPT_TIMEOUT => 5,
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => $job,
+            CURLOPT_HTTPHEADER => [
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($job)
+            ]
+        ]);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        try {
+            $result = json_decode($response);
+            return empty($result->id) ? false : $result->id;
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 }
